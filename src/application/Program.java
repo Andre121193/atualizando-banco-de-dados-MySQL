9b3783dcb0f9364e963fc2662f5d7f952bbf438f
Andre121193 <andre1993andre@hotmail.com>
2021-10-29 17:31:49 -0300

    aprendendo como atualizar banco de dados MySQL

---
+++ src/application/Program.java
@@ -0,0 +1,45 @@
+package application;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+
+import db.DB;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+	
+//==============UPDATE==============
+			
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
+			
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
+//==============Confirm it has been added==============
+			
+			int rowsAffected = st.executeUpdate();
+			
+			System.out.println("Done! Rows affected: " + rowsAffected);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		} 
+		finally {
+			DB.closeStatement((Statement) st);
+			DB.closeConnection();
+		}
+	}
+}
